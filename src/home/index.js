4d6b960c43b1767a4ee48f3dc94d297b8653067d
Walidnurudin <walidnurudin47@gmail.com>
2020-11-15 18:56:04 +0700

    50%

--- src/home/index.js
+++ src/home/index.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
+import axios from "axios";
+import { Container } from 'reactstrap';
 
 class Home extends Component {
     constructor() {
         super();
         this.state = {
-            data: []
+            name: "",
+            numbers: {},
+            regions: []
         }
     }
 
     componentDidMount() {
 
-        fetch("https://dekontaminasi.com/api/id/covid19/stats")
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
+        axios.get("https://dekontaminasi.com/api/id/covid19/stats")
+            .then(res => res.data)
+            .then(res => {
+                this.setState({
+                    name: res.name,
+                    numbers: res.numbers,
+                    regions: res.regions
+                })
+                console.log(this.state)
             })
             .catch(err => console.log("Error = " + err))
     }
@@ -21,9 +30,42 @@ class Home extends Component {
 
     render() {
         return (
-            <div>
-                <p>{this.state.data}, losadjasiojdiosjfiojsdiofj iodsjvi jidjs</p>
-            </div>
+            <Container>
+                <h1>{this.state.name}</h1>
+                <div>
+                    <h3>Positif : {this.state.numbers.infected}</h3>
+                    <h3>Sembuh : {this.state.numbers.recovered}</h3>
+                    <h3>Meninggal : {this.state.numbers.fatal}</h3>
+                </div>
+
+                <div class="table-responsive-sm">
+                    <table class="table">
+                        <caption>List of regions</caption>
+                        <thead>
+                            <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">Provinsi</th>
+                                <th scope="col">Positif</th>
+                                <th scope="col">Sembuh</th>
+                                <th scope="col">Meninggal</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.regions.map((data, index) => {
+                                return (
+                                    <tr>
+                                        <th scope="row">{index + 1}</th>
+                                        <td>{data.name}</td>
+                                        <td>{data.numbers.infected}</td>
+                                        <td>{data.numbers.recovered}</td>
+                                        <td>{data.numbers.fatal}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </Container>
         )
     }
 }
--- src/news/index.js
+++ src/news/index.js
@@ -1,24 +1,56 @@
 import React, { Component } from 'react';
-import { Button, Card, CardText, Container } from 'reactstrap';
+import { Button, Card, CardText, Container, Spinner } from 'reactstrap';
+import axios from "axios";
 
 class News extends Component {
     constructor() {
         super();
         this.state = {
-            news: []
+            news: [],
+            loading: false
         }
     }
 
+    componentDidMount() {
+        this.setState({ loading: true });
+        axios.get('https://dekontaminasi.com/api/id/covid19/news')
+            .then(res => res.data)
+            .then(res => {
+                this.setState({
+                    news: res,
+                    loading: false
+                })
+                console.log(this.state.news)
+            })
+            .catch(err => {
+                this.setState({ loading: false });
+                console.log(err);
+            })
+    }
+
     render() {
-        return (
-            <div>
-                <Container>
-                    <Card style={{width: '250px', padding: '10px'}}>
-                        <CardText>Joss! Banyak Pasien Covid-19 Sembuh, Kapasitas RSD Wisma Atlet Mulai Lega</CardText>
-                        <Button href="https://google.com" target="_blank">Detail</Button>
-                    </Card>
+        if (this.state.loading) {
+            return (
+                <Container style={{ textAlign: 'center' }}>
+                    <Spinner style={{ width: '3rem', height: '3rem', marginTop: '50px' }} type="grow" color="primary" />
                 </Container>
-            </div>
+            )
+        }
+        return (
+            <Container>
+                <div className="row">
+                    {this.state.news.map(data => {
+                        return (
+                            <div className="col d-flex justify-content-center">
+                                <Card style={{ width: '250px', padding: '10px', margin: '10px' }}>
+                                    <CardText>{data.title}</CardText>
+                                    <Button href={data.url} target="_blank">Detail</Button>
+                                </Card>
+                            </div>
+                        )
+                    })}
+                </div>
+            </Container>
         );
     }
 }
