fc9a9a84bbf269264290f105bf5f1742770fc3e1
Walidnurudin <walidnurudin47@gmail.com>
2020-11-15 20:27:59 +0700

    about

--- src/about/index.js
+++ src/about/index.js
@@ -9,21 +9,21 @@ class About extends Component {
                     <div className="col d-flex justify-content-center">
                         <Card style={{ width: '250px', padding: '10px', margin: '10px' }}>
                             <CardTitle>Developer</CardTitle>
-                            <CardText tag="h3">App made by <a href="https://walidnurudin.netlify.app" target="_blank">Walid nurudin</a></CardText>
+                            <CardText tag="h3">App made by <a href="https://walidnurudin.netlify.app" target="blank">Walid nurudin</a></CardText>
                         </Card>
                     </div>
 
                     <div className="col d-flex justify-content-center">
                         <Card style={{ width: '250px', padding: '10px', margin: '10px' }}>
                             <CardTitle>API</CardTitle>
-                            <CardText tag="h3">API from <a href="https://dekontaminasi.com"  target="_blank">Dekontaminasi API covid</a></CardText>
+                            <CardText tag="h3">API from <a href="https://dekontaminasi.com"  target="blank">Dekontaminasi API covid</a></CardText>
                         </Card>
                     </div>
 
                     <div className="col d-flex justify-content-center">
                         <Card style={{ width: '250px', padding: '10px', margin: '10px' }}>
                             <CardTitle>Icons</CardTitle>
-                            <CardText tag="h3">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik" target="_blank">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon" target="_blank"> www.flaticon.com</a></CardText>
+                            <CardText tag="h3">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik" target="blank">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon" target="blank"> www.flaticon.com</a></CardText>
                         </Card>
                     </div>
                 </div>
